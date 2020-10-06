68e399e83393a7544ed36aab848465bb3a0ac1c0
Renante D. Entera <renantee@codev.com>
2020-10-07 07:19:56 +0800

    Demo: Accept input from the command line
    Execute commands:
    > npm install inquirer
    > node input.js

---
+++ input.js
@@ -0,0 +1,25 @@
+// Using "readling"
+/*const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question(`What's your name?`, name => {
+  console.log(`Hi ${name}!`)
+  readline.close()
+})*/
+
+// Using "inquirer"
+const inquirer = require('inquirer')
+
+var questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your name?"
+  }
+]
+
+inquirer.prompt(questions).then(answers => {
+  console.log(`Hi ${answers['name']}!`)
+})
