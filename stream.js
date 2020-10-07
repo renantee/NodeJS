593b7c5fecebac6c266ef2faf29b8721a42fc11c
Renante D. Entera <renantee@codev.com>
2020-10-07 14:36:52 +0800

    Demo: Streams

---
+++ stream.js
@@ -0,0 +1,16 @@
+const Stream = require('stream')
+
+const readableStream = new Stream.Readable({
+  read() {}
+})
+const writableStream = new Stream.Writable()
+
+writableStream._write = (chunk, encoding, next) => {
+  console.log(chunk.toString())
+  next()
+}
+
+readableStream.pipe(writableStream)
+
+readableStream.push('hi!')
+readableStream.push('ho!')
