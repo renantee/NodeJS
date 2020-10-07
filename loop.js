d0209dce8c9fe0e9367f0399f9601a9d2b81678b
Renante D. Entera <renantee@codev.com>
2020-10-07 10:13:09 +0800

    Demo: Event loop

---
+++ loop.js
@@ -0,0 +1,45 @@
+// Basic loop
+/*
+const bar = () => console.log('bar')
+
+const baz = () => console.log('baz')
+
+const foo = () => {
+  console.log('foo')
+  bar()
+  baz()
+}
+
+foo()
+*/
+
+// Queuing loop
+/*
+const bar = () => console.log('bar')
+
+const baz = () => console.log('baz')
+
+const foo = () => {
+  console.log('foo')
+  setTimeout(bar, 0)
+  baz()
+}
+
+foo()
+*/
+
+// Job Queue loop
+const bar = () => console.log('bar')
+
+const baz = () => console.log('baz')
+
+const foo = () => {
+  console.log('foo')
+  setTimeout(bar, 0)
+  new Promise((resolve, reject) =>
+    resolve('should be right after baz, before bar')
+  ).then(resolve => console.log(resolve))
+  baz()
+}
+
+foo()
