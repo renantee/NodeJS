4c6c311c38851f2f94451a96eb3db1f5733a2c48
Renante D. Entera <renantee@codev.com>
2020-10-08 11:24:42 +0800

    Course Project: File system module

--- first-app/app.js
+++ first-app/app.js
@@ -1,12 +1,11 @@
-const os = require('os');
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// Synchronous method
+/*const files = fs.readdirSync('./');
+console.log(files);*/
 
-// console.log('Total Memory: ' + totalMemory);
-
-// Template string
-// ES6 / ES2015 : ECMAScript 6
-
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);
+// Asynchronous method
+fs.readdir('./', function(err, files) {
+  if (err) console.log('Error', err);
+  else console.log('Result', files)
+})
